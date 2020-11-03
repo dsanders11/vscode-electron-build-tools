ca27710983f117ecb1d1ac01d33533556c2f49ee
David Sanders <dsanders11@ucsbalum.com>
2020-11-02 19:30:16 -0800

    feat: move sync to task-style

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,10 @@
 import * as vscode from "vscode";
 import * as childProcess from "child_process";
 import * as fs from "fs";
-import * as net from "net";
 import * as path from "path";
-import * as readline from "readline";
 
 import { ElectronBuildToolsConfigsProvider } from "./configsView";
-import {
-  generateSocketName,
-  getConfigs,
-  getConfigsFilePath,
-  killThemAll,
-} from "./utils";
+import { getConfigs, getConfigsFilePath, runAsTask } from "./utils";
 
 async function electronIsInWorkspace(workspaceFolder: vscode.WorkspaceFolder) {
   const possiblePackageRoots = [".", "electron"];
@@ -44,101 +37,43 @@ function registerElectronBuildToolsCommands(
       const command = "electron-build-tools build";
       const operationName = "Electron Build Tools - Building";
 
-      const socketName = generateSocketName();
-
       const buildEnv = {
         ...process.env,
         NINJA_STATUS: "%p %f/%t ",
       };
 
-      const task = new vscode.Task(
-        { type: "electron-build-tools", task: "build" },
-        vscode.workspace.workspaceFolders![0],
+      let lastBuildProgress = 0;
+
+      runAsTask(
+        operationName,
         "build",
-        "electron-build-tools",
-        new vscode.ShellExecution(
-          `${command} | node tee-to-socket.js ${socketName}`,
-          { cwd: __dirname, env: buildEnv }
-        ),
-        "$electron"
-      );
-
-      // TODO - How to stop the terminal from being closed on task cancel?
-      task.presentationOptions = {
-        reveal: vscode.TaskRevealKind.Silent,
-        echo: false,
-        clear: true,
-      };
-
-      const socketServer = net.createServer().listen(socketName);
-
-      vscode.window.withProgress(
-        {
-          location: vscode.ProgressLocation.Notification,
-          title: operationName.split("-")[1].trim(),
-          cancellable: true,
-        },
-        async (progress, token) => {
-          let lastBuildProgress = 0;
-
-          socketServer.on("connection", (socket) => {
-            const rl = readline.createInterface({
-              input: socket,
+        command,
+        { env: buildEnv },
+        (progress, line) => {
+          if (/Regenerating ninja files/.test(line)) {
+            progress.report({
+              message: "Regenerating Ninja Files",
+              increment: 0,
             });
+          } else {
+            const buildProgress = parseInt(line.split("%")[0].trim());
 
-            rl.on("line", (line) => {
-              if (/Regenerating ninja files/.test(line)) {
+            if (!isNaN(buildProgress)) {
+              if (buildProgress > lastBuildProgress) {
                 progress.report({
-                  message: "Regenerating Ninja Files",
-                  increment: 0,
+                  message: "Compiling",
+                  increment: buildProgress - lastBuildProgress,
                 });
-              } else {
-                const buildProgress = parseInt(line.split("%")[0].trim());
-
-                if (!isNaN(buildProgress)) {
-                  if (buildProgress > lastBuildProgress) {
-                    progress.report({
-                      message: "Compiling",
-                      increment: buildProgress - lastBuildProgress,
-                    });
-                    lastBuildProgress = buildProgress;
-                  }
-                } else {
-                  if (/Running.*goma/.test(line)) {
-                    progress.report({ message: "Starting Goma" });
-                  } else if (/Running.*ninja/.test(line)) {
-                    progress.report({ message: "Starting" });
-                  }
-                }
+                lastBuildProgress = buildProgress;
               }
-            });
-          });
-
-          const taskExecution = await vscode.tasks.executeTask(task);
-
-          return new Promise(async (resolve, reject) => {
-            socketServer.on("error", () => reject("Socket server error"));
-
-            vscode.tasks.onDidEndTask(({ execution }) => {
-              if (execution === taskExecution) {
-                resolve();
+            } else {
+              if (/Running.*goma/.test(line)) {
+                progress.report({ message: "Starting Goma" });
+              } else if (/Running.*ninja/.test(line)) {
+                progress.report({ message: "Starting Ninja" });
               }
-            });
-
-            vscode.tasks.onDidEndTaskProcess(({ execution, exitCode }) => {
-              if (execution === taskExecution && exitCode && exitCode !== 0) {
-                vscode.window.showErrorMessage(
-                  `'${operationName}' failed with exit code ${exitCode}`
-                );
-              }
-            });
-
-            token.onCancellationRequested(() => {
-              resolve();
-              taskExecution.terminate();
-              console.warn(`User canceled '${command}'`);
-            });
-          }).finally(() => taskExecution.terminate());
+            }
+          }
         }
       );
     }),
@@ -210,60 +145,18 @@ function registerElectronBuildToolsCommands(
       const command = "electron-build-tools sync";
       const operationName = "Electron Build Tools - Syncing";
 
-      vscode.window.withProgress(
-        {
-          location: vscode.ProgressLocation.Notification,
-          title: operationName.split("-")[1].trim(),
-          cancellable: true,
-        },
-        (progress, token) => {
-          return new Promise((resolve) => {
-            const syncOperation = childProcess.exec(command);
-            let killed = false;
+      let initialProgress = false;
 
-            syncOperation.on("error", (error) => {
-              vscode.window.showErrorMessage(
-                `'${operationName}' had an error occur: ${error.message}`
-              );
-              resolve();
-            });
-
-            syncOperation.on("exit", (exitCode) => {
-              if (exitCode && !killed && exitCode !== 0) {
-                vscode.window.showErrorMessage(
-                  `'${operationName}' failed with exit code ${exitCode}`
-                );
-              }
-              resolve();
-            });
-
-            token.onCancellationRequested(() => {
-              killed = true;
-              killThemAll(syncOperation);
-              console.warn(`User canceled '${command}'`);
-              resolve();
-            });
-
-            const rl = readline.createInterface({
-              input: syncOperation.stdout!,
-            });
-
-            let initialProgress = false;
-
-            // TODO - Add more progress updates
-            rl.on("line", (line) => {
-              if (/running.*apply_all_patches\.py/.test(line)) {
-                progress.report({ message: "Applying Patches" });
-              } else if (/Hook.*apply_all_patches\.py.*took/.test(line)) {
-                progress.report({ message: "Finishing Up" });
-              } else if (!initialProgress) {
-                initialProgress = true;
-                progress.report({ message: "Dependencies" });
-              }
-            });
-          });
+      runAsTask(operationName, "sync", command, {}, (progress, line) => {
+        if (/running.*apply_all_patches\.py/.test(line)) {
+          progress.report({ message: "Applying Patches" });
+        } else if (/Hook.*apply_all_patches\.py.*took/.test(line)) {
+          progress.report({ message: "Finishing Up" });
+        } else if (!initialProgress) {
+          initialProgress = true;
+          progress.report({ message: "Dependencies" });
         }
-      );
+      });
     }),
     vscode.commands.registerCommand(
       "electron-build-tools.useConfig",
