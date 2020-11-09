a45522e88e42cb90aeea0c5bfd4e1e989bcf4680
David Sanders <dsanders11@ucsbalum.com>
2020-11-09 12:03:06 -0800

    feat: prettify markdown tables helper

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,8 @@ import { promisify } from "util";
 
 import * as vscode from "vscode";
 
+import MarkdownIt from "markdown-it";
+
 import {
   blankConfigEnumValue,
   buildTargets,
@@ -40,6 +42,12 @@ import {
   TestState,
   TestsTreeDataProvider,
 } from "./views/tests";
+
+enum MarkdownTableColumnAlignment {
+  LEFT,
+  CENTER,
+  RIGHT,
+}
 
 const exec = promisify(childProcess.exec);
 
@@ -726,6 +734,124 @@ function registerHelperCommands(context: vscode.ExtensionContext) {
 
         if (results) {
           return results[0].fsPath;
+        }
+      }
+    ),
+    vscode.commands.registerCommand(
+      "markdown.prettifyTable",
+      (uri: vscode.Uri) => {
+        if (
+          vscode.window.activeTextEditor &&
+          vscode.window.activeTextEditor.document.uri.path === uri.path
+        ) {
+          const selection = vscode.window.activeTextEditor.selection;
+          const document = vscode.window.activeTextEditor.document;
+          const selectedText = document.getText(
+            new vscode.Range(selection.start, selection.end)
+          );
+          const md = new MarkdownIt();
+          const tokens = md.parse(selectedText.trim(), {});
+
+          // TODO - Would be more robust to prettify the table off the parsed tokens
+          if (
+            tokens[0].type === "table_open" &&
+            tokens[tokens.length - 1].type === "table_close"
+          ) {
+            const tableRawLines = selectedText.split("\n");
+            const columnAlignments: MarkdownTableColumnAlignment[] = [];
+            const columnMaxLengths: number[] = [];
+            const table: string[][] = [];
+
+            for (const [lineNumber, line] of tableRawLines.entries()) {
+              const columns = line.split("|").map((column) => column.trim());
+              table.push(columns);
+
+              if (lineNumber !== 1) {
+                for (const [idx, column] of columns.entries()) {
+                  if (column.length > (columnMaxLengths[idx] || 0)) {
+                    columnMaxLengths[idx] = column.length;
+                  }
+                }
+              } else {
+                columnAlignments.push(
+                  ...columns.map((value) => {
+                    if (value.startsWith(":") && value.endsWith(":")) {
+                      return MarkdownTableColumnAlignment.CENTER;
+                    } else if (value.startsWith(":")) {
+                      return MarkdownTableColumnAlignment.LEFT;
+                    } else if (value.endsWith(":")) {
+                      return MarkdownTableColumnAlignment.RIGHT;
+                    } else {
+                      return MarkdownTableColumnAlignment.LEFT;
+                    }
+                  })
+                );
+              }
+            }
+
+            let prettiedTable = "";
+
+            for (const [lineNumber, line] of table.entries()) {
+              const prettiedColumns = [];
+
+              for (const [idx, column] of line.entries()) {
+                const alignment = columnAlignments[idx];
+                const targetLength = columnMaxLengths[idx];
+                let prettifiedColumn = "";
+
+                // The second line is a special case since it defines column alignment
+                if (lineNumber === 1) {
+                  switch (alignment) {
+                    case MarkdownTableColumnAlignment.LEFT:
+                      if (column.startsWith(":")) {
+                        prettifiedColumn = `:${"-".repeat(targetLength - 1)}`;
+                      } else {
+                        prettifiedColumn = "-".repeat(targetLength);
+                      }
+                      break;
+
+                    case MarkdownTableColumnAlignment.CENTER:
+                      prettifiedColumn = `:${"-".repeat(targetLength - 2)}:`;
+                      break;
+
+                    case MarkdownTableColumnAlignment.RIGHT:
+                      prettifiedColumn = `${"-".repeat(targetLength - 1)}:`;
+                      break;
+                  }
+                } else {
+                  switch (alignment) {
+                    case MarkdownTableColumnAlignment.LEFT:
+                      prettifiedColumn = column.padEnd(targetLength, " ");
+                      break;
+
+                    case MarkdownTableColumnAlignment.CENTER:
+                      const padLeft = Math.ceil(
+                        (targetLength - column.length) / 2
+                      );
+                      const padRight = targetLength - column.length - padLeft;
+                      prettifiedColumn = `${" ".repeat(
+                        padLeft
+                      )}${column}${" ".repeat(padRight)}`;
+                      break;
+
+                    case MarkdownTableColumnAlignment.RIGHT:
+                      prettifiedColumn = column.padStart(targetLength, " ");
+                      break;
+                  }
+                }
+
+                prettiedColumns.push(` ${prettifiedColumn} `);
+              }
+
+              prettiedTable += `${prettiedColumns.join("|").trim()}\n`;
+            }
+
+            vscode.window.activeTextEditor.edit((editBuilder) => {
+              editBuilder.replace(selection, prettiedTable.trim());
+            });
+          } else {
+            vscode.window.setStatusBarMessage("No markdown table selected");
+          }
         }
       }
     )
