23b0b3fedac96072b9d12f151d64230e4091b3d5
David Sanders <dsanders11@ucsbalum.com>
2020-11-18 23:51:36 -0800

    chore: move build command to separate file

--- src/extension.ts
+++ src/extension.ts
@@ -7,8 +7,6 @@ import MarkdownIt from "markdown-it";
 import MarkdownItEmoji from "markdown-it-emoji";
 
 import {
-  blankConfigEnumValue,
-  buildTargets,
   buildToolsExecutable,
   pullRequestScheme,
   virtualDocumentScheme,
@@ -19,16 +17,11 @@ import { DocsLinkablesProvider } from "./docsLinkablesProvider";
 import { setupDocsLinting } from "./docsLinting";
 import { GnLinkProvider } from "./gnLinkProvider";
 import Logger from "./logging";
-import { runAsTask } from "./tasks";
 import { TestCodeLensProvider } from "./testCodeLens";
-import { ExtensionConfig } from "./types";
 import {
   findElectronRoot,
-  getConfigDefaultTarget,
   getPatchesConfigFile,
   isBuildToolsInstalled,
-  registerCommandNoBusy,
-  withBusyState,
 } from "./utils";
 import {
   BuildToolsConfigCollector,
@@ -49,6 +42,7 @@ import { registerHelperCommands } from "./commands/helpers";
 import { registerConfigsCommands } from "./commands/configs";
 import { registerPatchesCommands } from "./commands/patches";
 import { registerSyncCommands } from "./commands/sync";
+import { registerBuildCommands } from "./commands/build";
 
 const exec = promisify(childProcess.exec);
 
@@ -62,6 +56,7 @@ function registerElectronBuildToolsCommands(
   testsCollector: TestCollector,
   pullRequestFileSystemProvider: ElectronPullRequestFileSystemProvider
 ) {
+  registerBuildCommands(context);
   registerConfigsCommands(context, configsProvider);
   registerTestCommands(context, electronRoot, testsProvider, testsCollector);
   registerPatchesCommands(
@@ -74,148 +69,6 @@ function registerElectronBuildToolsCommands(
   registerSyncCommands(context);
 
   context.subscriptions.push(
-    registerCommandNoBusy(
-      "electron-build-tools.build",
-      () => {
-        vscode.window.showErrorMessage("Can't build, other work in-progress");
-      },
-      () => {
-        return withBusyState(async () => {
-          const operationName = "Electron Build Tools - Building";
-
-          const buildConfig = vscode.workspace.getConfiguration(
-            "electronBuildTools.build"
-          );
-          const options = Object.entries(
-            buildConfig.get<ExtensionConfig.BuildOptions>("buildOptions")!
-          ).reduce((opts, [key, value]) => {
-            opts.push(`${key} ${value}`.trim());
-            return opts;
-          }, [] as string[]);
-          const ninjaArgs = Object.entries(
-            buildConfig.get<ExtensionConfig.NinjaArgs>("ninjaArgs")!
-          ).reduce((opts, [key, value]) => {
-            opts.push(`${key} ${value}`.trim());
-            return opts;
-          }, [] as string[]);
-
-          let settingsDefaultTarget = buildConfig.get<string>("defaultTarget");
-          settingsDefaultTarget =
-            settingsDefaultTarget === blankConfigEnumValue
-              ? ""
-              : settingsDefaultTarget;
-          let target = settingsDefaultTarget;
-
-          let quickPick: vscode.QuickPick<vscode.QuickPickItem> | undefined;
-
-          if (buildConfig.get<boolean>("showTargets")) {
-            // Settings default target takes precedence
-            const defaultTarget =
-              settingsDefaultTarget ?? (await getConfigDefaultTarget());
-            const quickPickItems: vscode.QuickPickItem[] = [];
-
-            if (defaultTarget) {
-              quickPickItems.push({
-                label: defaultTarget,
-                description: `Default from ${
-                  settingsDefaultTarget ? "Settings" : "Config"
-                }`,
-              });
-            } else {
-              quickPickItems.push({
-                label: "electron",
-                description: "Default",
-              });
-            }
-
-            for (const buildTarget of buildTargets) {
-              if (buildTarget !== quickPickItems[0].label) {
-                quickPickItems.push({
-                  label: buildTarget,
-                });
-              }
-            }
-
-            quickPick = vscode.window.createQuickPick();
-            quickPick.items = quickPickItems;
-            quickPick.placeholder = "Target To Build";
-          }
-
-          if (quickPick) {
-            const userQuit = await new Promise((resolve) => {
-              quickPick!.onDidAccept(() => {
-                target = quickPick!.selectedItems[0].label ?? target;
-                quickPick!.hide();
-                resolve();
-              });
-              quickPick!.onDidHide(() => {
-                resolve(true);
-              });
-              quickPick!.show();
-            });
-
-            if (userQuit) {
-              return;
-            }
-          }
-
-          const command = [
-            buildToolsExecutable,
-            "build",
-            ...options,
-            target,
-            ...ninjaArgs,
-          ]
-            .join(" ")
-            .trim();
-
-          const buildEnv = {
-            ...process.env,
-            NINJA_STATUS: "%p %f/%t ",
-          };
-
-          let lastBuildProgress = -1;
-
-          const task = runAsTask({
-            context,
-            operationName,
-            taskName: "build",
-            command,
-            shellOptions: { env: buildEnv },
-            problemMatchers: "$electron",
-          });
-
-          task.onDidWriteLine(({ progress, line }) => {
-            if (/Regenerating ninja files/.test(line)) {
-              progress.report({
-                message: "Regenerating Ninja Files",
-                increment: 0,
-              });
-            } else {
-              const buildProgress = parseInt(line.split("%")[0].trim());
-
-              if (!isNaN(buildProgress)) {
-                if (buildProgress > lastBuildProgress) {
-                  progress.report({
-                    message: "Compiling",
-                    increment: buildProgress - lastBuildProgress,
-                  });
-                  lastBuildProgress = buildProgress;
-                }
-              } else {
-                if (/Running.*goma/.test(line)) {
-                  progress.report({ message: "Starting Goma" });
-                } else if (/Running.*ninja/.test(line)) {
-                  progress.report({ message: "Starting Ninja" });
-                }
-              }
-            }
-          });
-
-          await task.finished;
-        });
-      }
-    ),
     vscode.commands.registerCommand(
       "electron-build-tools.show.exe",
       async () => {
