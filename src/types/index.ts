e09d42704d7d455c3085ee2b5a05fadcbe622663
David Sanders <dsanders11@ucsbalum.com>
2020-11-18 22:01:46 -0800

    chore: reorganize types to be easier to maintain

---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export { JSONSchemaForElectronPatchesConfigJson as ElectronPatchesConfig } from "./electronPatchesConfig";
+export { JSONSchemaForEVMConfigurationFiles as EVMConfig } from "./evmConfig";
+export * as ExtensionConfig from "./extensionConfig";
