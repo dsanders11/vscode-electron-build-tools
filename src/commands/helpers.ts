ec208ae6fc8d735d48f5060ac8065626ddbadc76
David Sanders <dsanders11@ucsbalum.com>
2020-11-24 00:21:37 -0800

    feat: markdown commands to wrap lines and rewrap selections

--- src/commands/helpers.ts
+++ src/commands/helpers.ts
@@ -1,11 +1,87 @@
 import * as vscode from "vscode";
 
 import MarkdownIt from "markdown-it";
+import wrap from "word-wrap";
+
+import Logger from "../logging";
 
 enum MarkdownTableColumnAlignment {
   LEFT,
   CENTER,
   RIGHT,
+}
+
+const indentedLineRegex = { regex: /^([ \t]*)(?=\S)/, linePrefix: "" };
+const orderedListRegex = {
+  regex: /^([ \t]*)\d+\.[ \t]*(?=\S)/,
+  linePrefix: "",
+};
+const unorderedListRegex = {
+  regex: /^([ \t]*)\*[ \t]*(?=[^\s\*])/,
+  linePrefix: "",
+};
+const quoteRegex = { regex: /^()\>[ \t]*(?=\S)/, linePrefix: ">" };
+// NOTE - Regexes are ordered such that the least specific, any indentation
+const wrapLineRegexes = [
+  orderedListRegex,
+  unorderedListRegex,
+  quoteRegex,
+  indentedLineRegex,
+];
+const targetLineLength = 80;
+
+function getLinesForSelection(
+  document: vscode.TextDocument,
+  selection: vscode.Selection,
+  allowEmptySelections = true
+) {
+  const lines: vscode.TextLine[] = [];
+
+  if (!selection.isEmpty) {
+    const { start, end } = selection;
+    const endLineEnd = document.lineAt(end).range.end;
+    const beginsAtStartOfLine = start.character === 0;
+    const endsAtEndOfLine = end.character === endLineEnd.character;
+
+    if (beginsAtStartOfLine && endsAtEndOfLine) {
+      for (let lineNum = start.line; lineNum <= end.line; lineNum++) {
+        lines.push(document.lineAt(lineNum));
+      }
+    } else {
+      return null;
+    }
+  } else if (allowEmptySelections) {
+    lines.push(document.lineAt(selection.active));
+  } else {
+    return null;
+  }
+
+  return lines;
+}
+
+function wrapLine(text: string) {
+  let initialIndent = "";
+  let indent = "";
+  text = text.trimRight();
+
+  for (const { regex, linePrefix } of wrapLineRegexes) {
+    if (regex.test(text)) {
+      const match = text.match(regex)!;
+      initialIndent = match[1];
+      indent = linePrefix + " ".repeat(match[0].length - linePrefix.length);
+      text = text.slice(linePrefix.length).trimLeft();
+      break;
+    }
+  }
+
+  return (
+    initialIndent +
+    wrap(text, {
+      indent,
+      trim: true,
+      width: 79 - indent.length,
+    }).trim()
+  );
 }
 
 export function registerHelperCommands(context: vscode.ExtensionContext) {
@@ -130,6 +206,68 @@ export function registerHelperCommands(context: vscode.ExtensionContext) {
           edit.replace(selection, prettiedTable.trim());
         } else {
           vscode.window.setStatusBarMessage("No markdown table selected");
+        }
+      }
+    ),
+    vscode.commands.registerTextEditorCommand(
+      "markdown.rewrapSelections",
+      (
+        { document, selections }: vscode.TextEditor,
+        edit: vscode.TextEditorEdit
+      ) => {
+        for (const selection of selections) {
+          const originalText = document.getText(selection);
+          const lines = getLinesForSelection(document, selection, false);
+
+          if (lines === null) {
+            Logger.warn("User tried to rewrap empty or partial line selection");
+            vscode.window.setStatusBarMessage(
+              "Can't rewrap empty or partial line selection"
+            );
+            continue;
+          }
+
+          const unwrappedText = lines.map((line) => line.text).join(" ");
+          const rewrappedText = wrapLine(unwrappedText);
+
+          if (rewrappedText !== originalText) {
+            edit.replace(selection, rewrappedText);
+          }
+        }
+      }
+    ),
+    vscode.commands.registerTextEditorCommand(
+      "markdown.wrapLines",
+      (
+        { document, selections }: vscode.TextEditor,
+        edit: vscode.TextEditorEdit
+      ) => {
+        for (const selection of selections) {
+          const lines = getLinesForSelection(document, selection);
+
+          if (lines === null) {
+            Logger.warn("User tried to wrap line with partial line selection");
+            vscode.window.setStatusBarMessage(
+              "Can't wrap line with partial line selection"
+            );
+            continue;
+          }
+
+          for (const line of lines) {
+            // Skip lines which don't need wrapping
+            if (
+              line.isEmptyOrWhitespace ||
+              line.range.end.character < targetLineLength
+            ) {
+              continue;
+            }
+
+            const wrappedText = wrapLine(line.text);
+
+            if (wrappedText !== line.text) {
+              edit.replace(line.range, wrappedText);
+            }
+          }
         }
       }
     )
